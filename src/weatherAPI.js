f1797bdddd63745d8005d92ffc313d25c323e060
Jacob <jperez32017@gmail.com>
2024-02-21 17:47:11 -0800

    fixing api url

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,7 +1,6 @@
 export default async function fetchWeather(city) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=03d984259a17467b965230209241201&q=${city}&days=5`,
-    { mode: "cors" }
+    `https://api.weatherapi.com/v1/forecast.json?key=03d984259a17467b965230209241201&q=${city}&days=5`
   );
 
   if (response.status === 400) {
